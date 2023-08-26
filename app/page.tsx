a3dfefd2f3ac9c747184aa56a21464543e05111d
Debaditya <debadityasingh9@gmail.com>
2023-08-26 16:22:51 +0530

    Authentication added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import {UserButton} from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h2>Hello</h2>
+      <UserButton/>
     </div>
   )
 }
