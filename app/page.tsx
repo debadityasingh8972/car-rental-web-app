0e13b41b2ee592db62a806c0ea6e1187c564709c
Debaditya <debadityasingh9@gmail.com>
2023-08-31 21:23:47 +0530

    Adding Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+"use client"
+
+import Hero from '@/components/Home/Hero'
 import Image from 'next/image'
 
 
 export default function Home() {
   return (
-    <div>
-      <h2>Hello</h2>
+    <div className='p-5 sm:px-10 md:px-20'>
+      <Hero/>
       
     </div>
   )
---
+++ components/Home/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-2'>
+            <div>
+                Text
+            </div>
+            <div>
+                IMage
+            </div>
+        </div>
+    )
+}
+
+export default Hero
