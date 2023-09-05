d7a69db68d4ddbbeac57dfe997f89b5330008107
Debaditya <debadityasingh9@gmail.com>
2023-09-05 23:52:27 +0530

    Creating Search filter page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Hero from '@/components/Home/Hero'
+import SearchInput from '@/components/Home/SearchInput'
 
 
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div className='p-5 sm:px-10 md:px-20'>
       <Hero/>
+      <SearchInput/>
       
     </div>
   )
